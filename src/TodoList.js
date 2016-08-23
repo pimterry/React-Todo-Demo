8d50fa817a744b73fb3275d7bdf1353970001ae2
Tim Perry <pimterry@gmail.com>
2016-08-23 15:27:32 +0200

    Allow adding todos to the list

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import './TodoList.css';
 
 class TodoInput extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = { content: '' };
+  }
+
+  handleContentChange(event) {
+    this.setState({ content: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (!this.state.content) return;
+
+    this.props.onTodoAdded(this.state);
+    this.setState({ content: '' });
+  }
+
   render() {
     return (
-      <div>Enter your TODO here</div>
+      <div className="todoInput">
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text"
+                 placeholder="What do you want to do?"
+                 value={this.state.content}
+                 onChange={this.handleContentChange.bind(this)} />
+          <input type="submit"
+                 value="Add Task" />
+        </form>
+      </div>
     )
   }
 }
@@ -18,13 +45,24 @@ class TodoItem extends Component {
 }
 
 class TodoList extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = { todos: [] };
+  }
+
+  addTodo(todo) {
+    var todos = this.state.todos.concat(todo);
+    this.setState({ todos: todos });
+  }
+
   render() {
     return (
-      <div className="TodoList">
-        <TodoInput/>
+      <div className="todoList">
+        <TodoInput onTodoAdded={this.addTodo.bind(this)} />
 
         <ul>
-        {this.props.todos.map((todo, i) => (
+        {this.state.todos.map((todo, i) => (
           <TodoItem content={todo.content} key={i} />
         ))}
         </ul>
--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import TodoList from './TodoList';
 import './index.css';
 
-var todos = [
-  { content: "A todo" },
-  { content: "Another todo" },
-  { content: "Todo or not todo" },
-  { content: "Todon't" }
-];
-
 ReactDOM.render(
-  <TodoList todos={todos} />,
+  <TodoList />,
   document.getElementById('root')
 );
