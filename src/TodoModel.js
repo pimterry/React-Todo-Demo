68523882e45ceb03ef04913325c8581d2cecc167
Tim Perry <pimterry@gmail.com>
2016-08-24 15:15:56 +0200

    Add indent/unindent to the model by refactoring to an immutable tree structure

--- src/TodoModel.js
+++ src/TodoModel.js
@@ -1,65 +1,111 @@
 // Immutable models for a set of todos and each single todo, to keep core logic and any mutation out of the views.
+// These could be done more efficiently, sticking with a simpler copy-frequently approach for now.
+// Could be done more simply mutably, but external API fits React much nicer this way
 
-export class TodoItems {
-    constructor(todos = []) {
-        this.todos = todos;
-    }
-
-    addTodo(newTodo) {
-        return new TodoItems(this.todos.concat(newTodo));
-    }
-
-    updateTodo(originalTodo, updatedTodo) {
-        return new TodoItems(this.todos.map((todo) => todo === originalTodo ? updatedTodo : todo));
-    }
-
-    getIncomplete() {
-        return this.todos.filter((t) => !t.completed);
-    }
-}
-
-export class TodoItem {
-    constructor(content, completed = false, parent = null, children = []) {
-        this.content = content;
-        this.completed = completed;
+class ImmutableTreeNode {
+    // Takes its state, and two callbacks to copy itself with a given change.
+    constructor(parent, children, setParent, setChildren) {
         this.parent = parent;
-        this.children = children;
-    }
+        // Parentage automatically propagates down when you change a node
+        this.children = children.map((c) => c.setParent(this));
 
-    updateContent(newContent) {
-        return copyWithChanges(this, { content: newContent });
-    }
-
-    toggleCompletion() {
-        return copyWithChanges(this, { completed: !this.completed });
-    }
-
-    setParent(newParent) {
-        return copyWithChanges(this, { parent: newParent });
+        this.setChildren = setChildren;
+        this.setParent = setParent;
     }
 
     addChild(newChild) {
-        return copyWithChanges(this, { children: this.children.concat(newChild) });
+        return this.setChildren(this.children.concat(newChild));
     }
 
-    removeChild(childToRemove) {
-        return copyWithChanges(this, { children: this.children.filter((c) => c !== childToRemove) });
+    updateChild(originalChild, newChild) {
+        return this.setChildren(this.children.map((child) => {
+            if (child === originalChild) return newChild;
+            else return child.updateChild(originalChild, newChild);
+        }));
+    }
+
+    // The tricky bit! Recurses down the tree, dropping the existing node when it is found,
+    // and adding it to the parent when that is found too.
+    // Note that if the parent is the child, or any descendent of the child, then this effectively
+    // just removes the child from the tree.
+    moveChild(originalChild, targetParent) {
+        var updatedChildren = this.children.filter((child) => {
+            return child !== originalChild;
+        }).map((child) => child.moveChild(originalChild, targetParent));
+
+        if (this === targetParent) updatedChildren.push(originalChild.setParent(this));
+
+        return this.setChildren(updatedChildren);
     }
 }
 
-// Convenience method to build a copy of a todo changing only the parameters provided
-function copyWithChanges(todo, changes) {
-    var constructorArgs = Object.assign({
-        content: todo.content,
-        completed: todo.completed,
-        parent: todo.parent,
-        children: todo.children
-    }, changes);
+export class TodoItems extends ImmutableTreeNode {
+    constructor(todos = []) {
+        super(null, todos, null, (newTodos) => new TodoItems(newTodos));
+    }
 
-    return new TodoItem(
-        constructorArgs.content,
-        constructorArgs.completed,
-        constructorArgs.parent,
-        constructorArgs.children
-    );
+    addTodo(newTodo) {
+        return this.addChild(newTodo);
+    }
+
+    updateTodo(originalTodo, updatedTodo) {
+        return this.updateChild(originalTodo, updatedTodo);
+    }
+
+    indentTodo(todoToIndent) {
+        var initialParent = todoToIndent.parent;
+        var todoIndex = initialParent.children.indexOf(todoToIndent);
+
+        // Do nothing if todo doesn't have a preceeding sibling
+        if (todoIndex < 1) return this;
+
+        var targetParent = initialParent.children[todoIndex - 1];
+        return this.moveChild(todoToIndent, targetParent);
+    }
+
+    unindentTodo(todoToUnindent) {
+        // Can't unindent nodes already at the root
+        if (todoToUnindent.parent === this) return this;
+        else return this.moveChild(todoToUnindent, todoToUnindent.parent.parent);
+    }
+
+    getIncomplete() {
+        return this.children.filter((t) => !t.completed);
+    }
+}
+
+export class TodoItem extends ImmutableTreeNode {
+    constructor(content, completed = false, parent = null, children = []) {
+        super(parent,
+              children,
+              (newParent) => this.copyWithChanges({ parent: newParent }),
+              (newChildren) => this.copyWithChanges({ children: newChildren }));
+        this.content = content;
+        this.completed = completed;
+    }
+
+    updateContent(newContent) {
+        return this.copyWithChanges({ content: newContent });
+    }
+
+    toggleCompletion() {
+        return this.copyWithChanges({ completed: !this.completed });
+    }
+
+    // Convenience method to build a copy of this todo, changing only the parameters provided
+    copyWithChanges(changes) {
+        var constructorArgs = Object.assign({
+            content: this.content,
+            completed: this.completed,
+            parent: this.parent,
+            children: this.children
+        }, changes);
+
+        return new TodoItem(
+            constructorArgs.content,
+            constructorArgs.completed,
+            constructorArgs.parent,
+            constructorArgs.children
+        );
+    }
 }
